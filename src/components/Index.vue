2f1fa68f9b5f16885355828e9ecf73cd17c7c011
zhuozj <774792044@qq.com>
2021-07-13 20:20:35 +0800

    add menu

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>
+      <el-header style="back">
         <el-row :gutter="20">
           <el-col :span="8" :offset="8" >
             <div>
@@ -24,6 +24,23 @@
       <el-main>
       <el-divider></el-divider> 
         main
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+</el-menu>
       </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
@@ -35,9 +52,16 @@ export default {
   data() {
     return {
       input: '',
-      select: ''
+      select: '',
+      activeIndex: '1',
+      activeIndex2: '1'
     }
-  }
+  },
+  methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
 }
 </script>
 <style>
