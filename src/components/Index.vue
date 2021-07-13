19ca72100c50d41f8ba1f33c75c21c13201a43ed
zhuozj <774792044@qq.com>
2021-07-13 20:15:49 +0800

    first co\mmit

---
+++ src/components/Index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="8" >
+            <div>
+              <el-input :offset="-1" placeholder="请输入URL" v-model="input" class="input-with-select">
+                <el-select v-model="select" slot="prepend" placeholder="http://">
+                  <el-option label="http://" value="http"></el-option>
+                  <el-option label="https://" value="https"></el-option>
+                </el-select>
+                <!-- <el-button type="primary" slot="append" icon="el-icon-search"></el-button> -->
+              </el-input>
+            </div>
+          </el-col>
+          <el-col :span="1" :offset="0">
+            <div style="position:absulute">
+              <el-button type="primary" icon="el-icon-search">搜索</el-button>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+      <el-divider></el-divider> 
+        main
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+      select: ''
+    }
+  }
+}
+</script>
+<style>
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  .el-select{
+    width: 130px;
+  }
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+</style>
+
+
